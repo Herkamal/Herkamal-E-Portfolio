117caa01ef3e560bd7af338747452ada0e2046d1
Herkamal <herkamalrandhawa@gmail.com>
2023-02-01 14:43:18 -0500

    contrast

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// template_g8prexom
-// service_sm7y51o
-// uChH4fEz-EULeoLue
+let isModalOpen = false
+let contrastToggle = false
+
+function toggleContrast(){
+  contrastToggle = !contrastToggle
+  if(contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else{
+    document.body.classList.remove('dark-theme')
+  }
+}
 
 function contact(event){
   event.preventDefault()
@@ -23,7 +32,6 @@ function contact(event){
       )
     })
   }
-  let isModalOpen = false
   function toggleModal(){
     if(isModalOpen){
       isModalOpen = false
